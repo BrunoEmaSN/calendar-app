a9a422a1da4921a162ffb9cd3d5e6f913f0e7b46
BrunoEmaSN <bruno.sanchez@dirmod.com>
2022-01-27 16:28:08 -0300

    ver islog

--- src/Routers/PublicRoute.js
+++ src/Routers/PublicRoute.js
@@ -7,6 +7,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 export const PublicRoute = ({
     isLogin
 }) => {
+    console.log(isLogin)
     return !isLogin ? <Outlet /> : <Navigate to ="/" />
 }
 
